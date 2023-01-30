d7389dc1f7de595874ebda0cd4161979a095a82c
Imon <contact@imonweb.co.uk>
2023-01-30 21:56:51 +0000

    deprecated v1

--- plugins-bak/text-box-bak/src/index.js
+++ plugins-bak/text-box-bak/src/index.js
@@ -3,11 +3,13 @@ import { __ } from '@wordpress/i18n';
 import './style.scss';
 import Edit from './edit';
 import save from './save';
+import v1 from './v1';
 
 registerBlockType('blocks-course/text-box', {
 
 	edit: Edit,
 	save,
+	deprecated: [ v1 ],
 	variations: [
 		{
 			name: 'blocks-course/gradient-text-box',
