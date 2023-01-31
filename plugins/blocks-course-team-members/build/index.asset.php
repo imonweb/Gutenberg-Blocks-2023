f46da2042a3c1788ab317080fff92609c815f891
Imon <contact@imonweb.co.uk>
2023-01-31 22:43:24 +0000

    storing and displaying team member images

--- plugins/blocks-course-team-members/build/index.asset.php
+++ plugins/blocks-course-team-members/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f8f442af44187ea255f5');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '55375eed9a31195d404f');
