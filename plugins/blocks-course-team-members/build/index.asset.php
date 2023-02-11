4e52733e61847028330d6bf1bb3a56aeddd97d0a
Imon <contact@imonweb.co.uk>
2023-02-11 23:52:51 +0000

    images

--- plugins/blocks-course-team-members/build/index.asset.php
+++ plugins/blocks-course-team-members/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7f9b0038ac398cbd19e5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7b8bd2d65e173549535d');
